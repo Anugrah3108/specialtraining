a2d7d816a6979ef0e836c0a106c4c5bd5161fefc
Anugrah <anugrahsingh3108@gmail.com>
2024-02-06 13:57:35 +0530

    Adding index

--- Assignment-2/script.js
+++
@@ -1,8 +0,0 @@
-async function fetchData() {
-    const ret = await fetch("https://api.github.com/users/anugrah3108");
-    const data = await ret.text();
-    document.getElementById("dat").innerHTML = data;
-}
-document.getElementById("clickHere").addEventListener("click", event => {
-    fetchData();
-})
